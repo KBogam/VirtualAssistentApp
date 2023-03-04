0b94838b88f21dd86f3d31025d38ac59bbbc0eaa
KBogam <80017179+KBogam@users.noreply.github.com>
2023-03-04 11:47:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function send()
+{
+
+    var TextSent = document.getElementById("TextSent").value;
+
+    document.getElementById("YourText").innerHTML = "Message Should Be Sent To ChatGPT: "+TextSent ;
+    Strike = 0;
+
+}
+
+
